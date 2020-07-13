dcda0fd7be58b81d064a5e5c6e6b041aae14e83d
feebeef <phoebe_chen@dlsu.edu.ph>
2020-07-13 19:56:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = new express();
 
 mongoose.Promise = global.Promise
 
-const MONGOLAB_URI = "mongodb+srv://user1:okay@cluster0.diqll.mongodb.net/appointments?retryWrites=true&w=majority";
+const MONGOLAB_URI = "mongodb+sv://dbUser:dbUserPassword@cluster0-shard-00-00-sjlw8.mongodb.net:27017,cluster0-shard-00-01-sjlw8.mongodb.net:27017,cluster0-shard-00-02-sjlw8.mongodb.net:27017/Test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose.connect(MONGOLAB_URI, {
     useNewUrlParser: true,
