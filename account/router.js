e90cdb623b893c2caf0f7079e14384a84d8134cc
Nate3447 <nathaniel.sierras@gmail.com>
2020-07-23 19:37:15 +0800

    Added header for router.js

---
+++ account/router.js
@@ -0,0 +1,8 @@
+
+const express = require("express");
+const router = express.Router();
+const moment = require('moment');
+
+const {Account} = require("../model/account");
+
+module.exports = router;
