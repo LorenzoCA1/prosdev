bb866b7ee8002e05d8c08c7492f27f16a4b69dcf
KTagle <kyletagle727@gmail.com>
2020-07-27 19:08:47 +0800

    edit doctor init

--- automation/function_test/DoctorFunctionAutomation.py
+++ automation/function_test/DoctorFunctionAutomation.py
@@ -4,13 +4,19 @@ import unittest
 import time
 
 class DoctorFunction(unittest.TestCase):
-    def setUp(self):
+    '''def setUp(self):
         self.driver = webdriver.Chrome("D:\\Programs\\PROSDEV\\prosdev\\automation\\chromedriver.exe")
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
-        self.driver.get("localhost:3000")
+        self.driver.get("localhost:3000")'''
+    @classmethod    
+    def setUpClass(inst):
+        inst.driver = webdriver.Chrome("D:\\Programs\\PROSDEV\\prosdev\\automation\\chromedriver.exe")
+        inst.driver.implicitly_wait(30)
+        inst.driver.maximize_window()
+        inst.driver.get("localhost:3000")
 
-    def test_1_add_doctor_first(self):
+    def test_1_add_doctor(self):
         print("Adding Doctors Test")
         self.driver.implicitly_wait(10)
         self.addBtn = self.driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[1]/span[1]/i")
@@ -23,9 +29,17 @@ class DoctorFunction(unittest.TestCase):
         self.driver.refresh()
         self.doctorList = self.driver.find_elements_by_name("UNITTEST")
         assert self.doctorList[0].text == "UNITTEST"
+
+    def test_2_edit_doctor(self):
+        print("Edit Doctors Test")
+        self.driver.implicitly_wait(10)
+        self.editBtn = self.driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[1]/span[2]")
+        self.editBtn.click()
+        
     
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDownClass(inst):
+        inst.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
