faeae06d4a1c8b59017cede187ebd326619ef3ea
KTagle <kyletagle727@gmail.com>
2020-07-27 16:52:07 +0800

    add doctor automation unit test

--- automation/ui_test/NavigationAutomation.py
+++ automation/ui_test/NavigationAutomation.py
@@ -11,14 +11,14 @@ class SimpleNavigation(unittest.TestCase):
         inst.driver.maximize_window()
         inst.driver.get("localhost:3000")
 
-    def test_navigate_to_site(self):
+    def test_1_navigate_to_site_(self):
         print("Navigation Test")
         #self.title = self.driver.title
         self.driver.implicitly_wait(30) #seconds
         self.appointmentName = self.driver.find_element_by_xpath("/html/body/nav/a[1]")
         assert self.appointmentName.text == "Appointment"
 
-    def test_add_doctor_popup(self):
+    def test_2_add_doctor_popup(self):
         print("Doctor Lists PopUp Test")
         self.driver.implicitly_wait(10)
         self.addBtn = self.driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[1]/span[1]/i")
@@ -28,7 +28,7 @@ class SimpleNavigation(unittest.TestCase):
         print(self.doctorWindow[0].text)
         assert self.doctorWindow[0].text == "Doctors"
     
-    def test_add_process_popup(self):
+    def test_3_add_process_popup(self):
         print("Proccess Add PopUp Test")
         self.driver.implicitly_wait(10)
         self.addBtn = self.driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div[1]/span[1]/i")
