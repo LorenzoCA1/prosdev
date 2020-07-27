aecaebde74f2d6bd3a8908049c0b6ec4d685e9c5
feebeef <phoebe_chen@dlsu.edu.ph>
2020-07-27 20:31:01 +0800

    login view and route

--- auth-account/router.js
+++ auth-account/router.js
@@ -3,8 +3,11 @@ const express = require("express");
 const router = express.Router();
 const moment = require('moment');
 
-const {Account} = require("../model/account");
+//const {Account} = require("../model/account");
 
+router.get("/auth", async function(req, res) {
+    res.render('login.hbs');
+});
 
 
 module.exports = router;
