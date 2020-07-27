7fc534555a0fdbc202552cbad79ec5247aab7977
KTagle <kyletagle727@gmail.com>
2020-07-27 14:38:24 +0800

    fixed paths and xpath

--- automation/selenium-python-test.py
+++ automation/selenium-python-test.py
@@ -5,16 +5,17 @@ import time
 
 class SimpleNavigation(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome("D:\\Programs\\PROSDEV\\prosdev\\unit-tests\\automation\\chromedriver.exe")
+        self.driver = webdriver.Chrome("D:\\Programs\\PROSDEV\\prosdev\\automation\\chromedriver.exe")
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
 
-    def test_navigate(self):
+    def test_navigate_to_site(self):
         print("Navigation Test")
-        self.driver.get("localhost:3000/secretary")
-        self.title = self.driver.title
-        assert "Secretary" in self.title
-        time.sleep(10)
+        self.driver.get("localhost:3000")
+        #self.title = self.driver.title
+        self.driver.implicitly_wait(30) #seconds
+        self.appointmentName = self.driver.find_element_by_xpath("/html/body/nav/a[1]")
+        assert self.appointmentName.text == "Appointment"
     
     def teardown(self):
         self.driver.quit()
