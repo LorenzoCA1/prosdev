23efb886ca0f4864f7e853b4b99cb21d281aa1c4
Nate3447 <nathaniel.sierras@gmail.com>
2020-08-01 22:39:37 +0800

    Protected secretary appointment page

--- app-secretary/router.js
+++ app-secretary/router.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const moment = require('moment');
+const auth = require("../auth-account/auth.js");
 
 const {Appointment} = require("../model/appointment");
 const {Doctor} = require("../model/doctor");
@@ -11,7 +12,7 @@ const bad_request = (err, res) => {let x = ''; if(err)for(field in err.errors) x
                                      console.log(err); res.status(400).send(x)}
 const ok_request = (data, res) => { console.log("sending back to client"); console.log(data); res.status(300).send(data) }
 
-router.get("/", async function(req, res) {
+router.get("/", isSecretary, async function(req, res) {
     let doctors =   await Doctor.getAll();
     let processes = await Process.getAll();
 
