4c0eacee0ed9e6614f97fb55f97f906ae90a1341
Nate3447 <nathaniel.sierras@gmail.com>
2020-09-06 01:32:03 +0800

    Admin app files created

---
+++ app-admin/router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const moment = require('moment');
+const auth = require("../auth-account/auth.js");
+
+const {Appointment} = require("../model/appointment");
+const {Doctor} = require("../model/doctor");
+const {Process} = require("../model/process");
+
+const bad_request = (err, res) => {let x = ''; if(err)for(field in err.errors) x += (err.errors[field].message + '-')
+                                     console.log(err); res.status(400).send(x)}
+const ok_request = (data, res) => { console.log("sending back to client"); console.log(data); res.status(300).send(data) }
